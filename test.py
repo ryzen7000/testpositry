858b15defb0bbc438e02b3ec8a40ccc1d4930101
Keshav <keshava0208@gmail.com>
2023-06-24 14:33:40 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,16 @@
+class keshav(object):
+  def __init__(self):
+    self.items=[]
+
+  def mamey(self):
+    marks={"K":[100,97,98],"M":[97,98,89],"A":[35,67,80]}
+    queriname = input("Enter name : ")
+    a = 0
+    for i in marks[queriname]:
+      i=i+a
+      a=i
+    return format(i/3,".2f")
+
+
+kholi = keshav()
+print(kholi.mamey())
